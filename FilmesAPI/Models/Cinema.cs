0a45d2b4954df267e1ac53ca52c5dda621122877
Carlos Tavares <carlostavares2350@gmail.com>
2023-08-31 16:54:35 -0300

    Criando o Medelo Cinema e os seu Dtos(create,read,updat,delete

---
+++ FilmesAPI/Models/Cinema.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Cinema
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="O campo é obrigatório!")]
+    public string Nome { get; set; }
+
+}
